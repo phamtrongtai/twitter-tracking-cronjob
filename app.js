ff8d141295480e4e6edc6d244550b7b11a712ad0
phamtrongtai <phamtrongtai@users.noreply.github.com>
2023-08-10 15:08:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var cron = require('node-cron');
 const https = require('https');
-
+const http = require('http');
 cron.schedule(`*/10 * * * * *`, () => {
   console.log(new Date(), 'running a task every 10s');
 https.get('https://twitter-tracking.vercel.app/api/users', res => { 
@@ -9,3 +9,11 @@ https.get('https://twitter-tracking.vercel.app/api/users', res => {
   });
 
 });
+
+// Use port number from the PORT environment variable or 3000 if not specified
+const port = process.env.PORT || 3000;
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('X!');
+    res.end();
+  }).listen(port);
